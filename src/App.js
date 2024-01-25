080cae362f573f4c6ded2bbb0381aab7d21200db
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-01-25 20:39:32 +0300

    skillsync

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import Header from './header/Header';
-import Home from './Component/Home';
-
+import "./App.css"
+import Header from './Component/common/header/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from './Component/about/About'
+import CourseHome from './Component/allcourses/CourseHome'
+import Team from './Component/team/Team'
+import Pricing from './Component/pricing/Pricing'
+import Blog from './Component/blog/Blog'
+import Contact from './Component/contact/contact'
+import Footer from './Component/common/footer/Footer'
+import Home from './Component/home/Home'
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        <Routes>
+        <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
+        </Routes>
+         
+        <Footer />
+      </Router>
+    </>
+  )
 }
-export default App;
+
+export default App
--- src/Component/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Home.css"
-import Header from '../header/Header';
-
-export default function Home() {
-  return (
-    <Header/>
-  )
-}
