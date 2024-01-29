18571c706bf02919438de0f945a9fe0b3250f30a
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-01-29 10:24:59 +0300

    portifolio

--- src/Component/common/header/Head.jsx
+++ src/Component/common/header/Head.jsx
@@ -11,16 +11,19 @@ const Head = () => {
             <h1>Skillsync</h1>
             <span>ONLINE Mentorship</span>
           </div>
+
+
           <div className="button">
-              <button>Singin/SignUp</button>
+              SignIn/SignUp
             </div>
 
-          <div className='social'>
+
+          {/* <div className='social'>
             <i className='fab fa-facebook-f icon'></i>
             <i className='fab fa-instagram icon'></i>
             <i className='fab fa-twitter icon'></i>
             <i className='fab fa-youtube icon'></i>
-          </div> 
+          </div>  */}
         </div>
       </section>
     </>
