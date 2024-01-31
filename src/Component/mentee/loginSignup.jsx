c1ccec23f0242eb8aa2fe8e081ebd5acfa105798
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-01-31 09:15:55 +0300

    portifolio

--- src/Component/mentee/loginSignup.jsx
+++ src/Component/mentee/loginSignup.jsx
@@ -1,41 +1,96 @@
-import React, { useState } from 'react'
-import './loginSignup.css'
 
-import user_icon from '../assets/person_FILL0_wght400_GRAD0_opsz24.png'
-import email_icon from '../assets/mail_FILL0_wght400_GRAD0_opsz24.png'
-import password_icon from '../assets/lock_FILL0_wght400_GRAD0_opsz24.png'
+import React, { useState } from 'react';
+import './loginSignup.css';
+
+import { getAuth } from '../firebase/app';
+import { auth } from '../firebase/app';
+import 'firebase/auth';
+
+import user_icon from '../assets/person_FILL0_wght400_GRAD0_opsz24.png';
+import email_icon from '../assets/mail_FILL0_wght400_GRAD0_opsz24.png';
+import password_icon from '../assets/lock_FILL0_wght400_GRAD0_opsz24.png';
+
 
 export default function LoginSignup() {
     const [action, setAction] = useState("Sign Up")
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleAuthentication = async () => {
+      try {
+        if (action === 'Sign Up') {
+          await auth.createUserWithEmailAndPassword(email, password);
+        } else {
+          await auth.signInWithEmailAndPassword(email, password);
+        }
+        console.log('Authentication successful');
+      } catch (error) {
+        console.error('Authentication failed', error.message);
+      }
+    }
+
   return (
     <div className="container">
-       <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-            {action==="Login" ? <div></div> : <div className="input">
-                    
-                    <img src={user_icon} alt="" />
-                    <input type="text" placeholder='name' required />
-                </div>}
-                
-                <div className="input">
-                    <img src={email_icon} alt="" />
-                    <input type="email" placeholder='Email' required='@gmail.com' />
-                </div>
-                <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder='Password' />
-                </div>
-                {action === "Sign Up" ? <div></div>:<div className="forgot-password">
-                    Lost Password
-                    <span>Click here</span></div>}            
-                <div className="submit-container">
-                    <div className= {action === "Login" ? "submit gray":"submit"} onClick={()=>{(setAction("Sign Up"))}} >Sign Up</div>
-                    <div className= {action === "Sign Up"? "submit gray":"submit"} onClick={()=>{(setAction("Login"))}} >Log In</div>
-                </div>
-            </div>
-        </div>             
+
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+
+      <div className="inputs">
+        {action === 'Login' ? null : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="name" required />
+          </div>
+        )}
+
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+
+        {action === 'Sign Up' ? null : (
+          <div className="forgot-password">
+            Lost Password
+            <span>Click here</span>
+          </div>
+        )}
+        <div className="submit-container">
+          <div
+            className={action === 'Login' ? 'submit gray' : 'submit'}
+            onClick={() => handleAuthentication()}
+          >
+            Sign Up
+          </div>
+          <div
+            className={action === 'Sign Up' ? 'submit gray' : 'submit'}
+            onClick={() => handleAuthentication()}
+          >
+            Log In
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
+}
+
