f206b954e883d553f999dcf3bfc8b1a61551de89
Alex Kinyua <alexkinyua001alx@gmail.com>
2024-02-07 19:16:18 +0300

    Revert "portifolio"
    This reverts commit 755e1fe4aa6117f8d246be313f8131080568409b.

--- src/Component/team/Team.jsx
+++ src/Component/team/Team.jsx
@@ -5,114 +5,57 @@ import "./team.css"
 
 
 import dure from "./image/Duresa.jpg"
-import bag from "./image/backGd.jpg"
-import facebok from "./image/facebook.png"
-import youTobe from "./image/youTobe.png"
-import whatup from "./image/whatup.png"
 
 export default function Team() {
   return (
     <>
        <Back title='Meet our Team' />
        <section className='team padding'>
-        <div className="container">
-          <h1 className="heading">MEET THE TEAM</h1>
-          <div className="card-warpper">
+         <div className='container grid'>
 
-            
-            <div className="card">
-              <img src={bag} alt="card background" className="card-img" />
-              <img src={dure} alt="profile image" className="profile-img" />
-              <h1>Duresa Eshetu</h1>
-              <p className="job-title">Web Developer</p>
-              <p className="about">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Eius possimus excepturi magnam perspiciatis corrupti? Ullam, 
-                quam asperiores pariatur quia amet ducimus impedit aspernatur 
-                voluptas ex odit at, qui excepturi laboriosam?
-              </p>
-              <a href="#" className="btn">Contact</a>
-              <ul className="social-media">
-                <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-facebook-f"><img src={facebok} alt="" /></i> </a> </li>
-               
-               <li> 
-                  <a href=""> <i className="fab fa-youTobe-f"><img src={youTobe} alt="" /></i> </a> </li>
+           <div className="Duresa">
+            <img src={dure} />
+            <h1>Duresa Eshetu</h1>
+            <h3>Full stack and Mining Engineer</h3>
+            <div className="lineShow"></div>
 
-                
-                <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-whatsapp-f"><img src={whatup} alt="" /></i> </a> </li>
-              </ul>
-            </div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+              Asperiores iste voluptatibus non ducimus. Voluptatem ex suscipit repudiandae, 
+              dignissimos ipsum perferendis magnam voluptates aliquid debitis odio minus eveniet
+               rerum saepe accusamus!
+               </p>
 
-            <div className="card">
-              <img src={bag} alt="card background" className="card-img" />
-              <img src={dure} alt="profile image" className="profile-img" />
-              <h1>Duresa Eshetu</h1>
-              <p className="job-title">Web Developer</p>
-              <p className="about">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Eius possimus excepturi magnam perspiciatis corrupti? Ullam, 
-                quam asperiores pariatur quia amet ducimus impedit aspernatur 
-                voluptas ex odit at, qui excepturi laboriosam?
-              </p>
-              <a href="#" className="btn">Contact</a>
-              <ul className="social-media">
-                <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-facebook-f"><img src={facebok} alt="" /></i> </a> </li>
-               
-               <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-youTobe-f"><img src={youTobe} alt="" /></i> </a> </li>
-
-                
-                <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-whatsapp-f"><img src={whatup} alt="" /></i> </a> </li>
-              </ul>
-            </div>
+           </div>
 
 
-            <div className="card">
-              <img src={bag} alt="card background" className="card-img" />
-              <img src={dure} alt="profile image" className="profile-img" />
-              <h1>Duresa Eshetu</h1>
-              <p className="job-title">Web Developer</p>
-              <p className="about">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Eius possimus excepturi magnam perspiciatis corrupti? Ullam, 
-                quam asperiores pariatur quia amet ducimus impedit aspernatur 
-                voluptas ex odit at, qui excepturi laboriosam?
-              </p>
-              <a href="#" className="btn">Contact</a>
-              <ul className="social-media">
-                <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-facebook-f"><img src={facebok} alt="" /></i> </a> </li>
-               
-               <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-youTobe-f"><img src={youTobe} alt="" /></i> </a> </li>
+           <div className="Duresa">
+            <img src={dure} />
+            <h1>Duresa Eshetu</h1>
+            <h3>Full stack and Mining Engineer</h3>
+            <div className="lineShow"></div>
 
-                
-                <li> 
-                  <a href=""> <i style={
-                  {color: "blue"}
-                } className="fab fa-whatsapp-f"><img src={whatup} alt="" /></i> </a> </li>
-              </ul>
-            </div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+              Asperiores iste voluptatibus non ducimus. Voluptatem ex suscipit repudiandae, 
+              dignissimos ipsum perferendis magnam voluptates aliquid debitis odio minus eveniet
+               rerum saepe accusamus!
+               </p>
 
-          </div>
+           </div>
+
+
+           <div className="Duresa">
+            <img src={dure} />
+            <h1>Duresa Eshetu</h1>
+            <h3>Full stack and Mining Engineer</h3>
+            <div className="lineShow"></div>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+              Asperiores iste voluptatibus non ducimus. Voluptatem ex suscipit repudiandae, 
+              dignissimos ipsum perferendis magnam voluptates aliquid debitis odio minus eveniet
+               rerum saepe accusamus!
+               </p>
+           </div>
+
          </div>
        </section>
     
