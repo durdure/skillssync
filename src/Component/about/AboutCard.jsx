a525255411d8735c50bdaf3718768e4d0d505e7d
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-02-10 16:53:56 +0300

    portifolio

--- src/Component/about/AboutCard.jsx
+++ src/Component/about/AboutCard.jsx
@@ -2,6 +2,18 @@ import React from "react"
 import "./about.css"
 import { homeAbout } from "../../dummydata"
 
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 import second from "./teacher1-removebg-preview.png"
 
 
@@ -26,13 +38,51 @@ const AboutCard = () => {
       <div className="team-heading">
         <h3>Our Top Mentor</h3>
       </div>
-      <div className="team-box">
+        <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 3,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+         <SwiperSlide className="swipper-slide">
+         <div className="team-box">
         <div className="b-t-image">
           <img src={second} alt=""
           style={
             {
               width:"500px",
-              marginTop:"30px"
+              marginTop:"30px",
+              paddingLeft:"150px"
+            }
+          }
+          />
+        </div>
+        <div className="b-t-text">
+          <strong>Julia Wi</strong>
+          <span>AI Engineer</span>
+        </div>
+      </div>
+        </SwiperSlide> 
+
+        <SwiperSlide className="swipper-slide">
+         <div className="team-box">
+        <div className="b-t-image">
+          <img src={second} alt=""
+          style={
+            {
+              width:"500px",
+              marginTop:"30px",
+              paddingLeft:"150px"
             }
           }
           />
@@ -42,6 +92,32 @@ const AboutCard = () => {
           <span>AI Engineer</span>
         </div>
       </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide className="swipper-slide">
+         <div className="team-box">
+        <div className="b-t-image">
+          <img src={second} alt=""
+          style={
+            {
+              width:"500px",
+              marginTop:"30px",
+              paddingLeft:"150px"
+            }
+          }
+          />
+        </div>
+        <div className="b-t-text">
+          <strong>Julia William</strong>
+          <span>AI Engineer</span>
+        </div>
+      </div>
+        </SwiperSlide>
+
+
+      </Swiper>
+
     </section>
     </>
   )
